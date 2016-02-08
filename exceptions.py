4a04adfc8fa54f72ba500e79b52b992a9642c242
Zolily <fre0003@jmss.vic.edu.au>
2016-02-08 15:03:00 +1100

    Adding an interger error exception

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,19 @@
 my_list = [1,2,3,5]
 
-try:
-    x = my_list[5]
-except IndexError:
-    print("Out of Range")
+#try:
+x = my_list[5]
+#except IndexError:
+#    print("Out of Range")
+notNum = True
+while notNum:
+    try:
+        x = input ("Please enter a whole number")
+
+        x = int(x)
+        notNum = False
+    except ValueError:
+        print("Hey! I asked you for a number!! Not a ",x)
+
+Print ("x is", x)
+
+total = x/0
