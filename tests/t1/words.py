55e8b2066f546ed2e04bf47efaf254a4b45341ab
Zolily <fre0003@jmss.vic.edu.au>
2016-02-29 15:07:28 +1100

    Stringystuff update

--- tests/t1/words.py
+++ tests/t1/words.py
@@ -1,18 +1,24 @@
 # Write a program to read in words from the keyboard one at a time until the word "quit" is typed.
 # Store them in a list then print them alphabetically
+wordlist = []
 
-wordlist=[]
-word = ""
 
-while word!="quit":
-    word = input("please enter a word: ")
-    wordlist.append(word)
 
-#wordlist.remove("quit")
-wordlist=wordlist[:-1]
 
-wordlist.sort()
-for word in wordlist:
-    print(word)
 
-print (wordlist)
+FinalWord = True
+while FinalWord:
+    response = input("Please Enter a word")
+    if "quit" in response.lower():
+        FinalWord = False
+        print(wordlist)
+
+    else:
+        wordlist = wordlist, response
+
+
+
+
+
+
+
