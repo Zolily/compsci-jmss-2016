bb6e3d592f16d1a92fcb202d42e0db9f7766b08f
Zolily <fre0003@jmss.vic.edu.au>
2016-02-29 15:07:28 +1100

    Stringystuff update

--- tests/t1/words.py
+++ tests/t1/words.py
@@ -1,2 +1,24 @@
 # Write a program to read in words from the keyboard one at a time until the word "quit" is typed.
 # Store them in a list then print them alphabetically
+wordlist = []
+
+
+
+
+
+FinalWord = True
+while FinalWord:
+    response = input("Please Enter a word")
+    if "quit" in response.lower():
+        FinalWord = False
+        print(wordlist)
+
+    else:
+        wordlist = wordlist, response
+
+
+
+
+
+
+
