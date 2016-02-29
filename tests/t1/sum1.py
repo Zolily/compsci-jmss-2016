e5798ad6478442b382bb15065951b66984a26331
Zolily <fre0003@jmss.vic.edu.au>
2016-02-29 15:09:35 +1100

    Stringystuff update

--- tests/t1/sum1.py
+++ tests/t1/sum1.py
@@ -1,1 +1,12 @@
 # write a program that reads in 10 numbers, then prints the sum of those
+sum = []
+sum = int()
+for i in range (10):
+    response = input("Please type a number")
+
+    sum = int(response) + sum
+
+print(sum)
+
+
+
--- tests/t1/sum2.py
+++ tests/t1/sum2.py
@@ -1,2 +1,15 @@
 # copy the code from sum1.py into this file, THEN:
 # change your program so it keeps reading numbers until it gets a -1, then prints the sum of all numbers read
+sum = []
+sum = int()
+
+
+NotDoneNumber = True
+while NotDoneNumber:
+    response = input("Please type a number")
+    if "-1" in response:
+        print(sum)
+        NotDoneNumber = False
+
+    else:
+        sum = int(response) + sum
